55102dc4cea601418a332e63bd445153f5b2cc5e
dadaewqq <dadaewqq@gmail.com>
2020-07-21 14:15:00 +0800

    优化显示

--- app/src/main/java/com/modosa/unblockdarkmode/fragment/XFeatureFragment.java
+++ app/src/main/java/com/modosa/unblockdarkmode/fragment/XFeatureFragment.java
@@ -138,6 +138,13 @@ public class XFeatureFragment extends PreferenceFragmentCompat implements Prefer
             preferences[i].setOnPreferenceClickListener(this);
         }
 
+        // Preference "x_wechat_hookBrand"
+        if(Build.VERSION.SDK_INT<=Build.VERSION_CODES.N_MR1){
+            findPreference("x_wechat_hookBrand").setSummary("建议您打开此项并将自定义品牌留空以达到最佳效果");
+        }
+
+
+
         update_hook_config.setOnPreferenceClickListener(this);
 
 
